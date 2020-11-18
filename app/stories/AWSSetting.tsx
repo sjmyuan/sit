b2718d836f5b9cac9f7ff066e2a5cc4adceb87ea
Jiaming Shang <shangjiaming.yuan@gmail.com>
2020-11-18 13:20:05 +0800

    add more scenario for aws settings

--- app/stories/AWSSetting.tsx
+++ app/stories/AWSSetting.tsx
@@ -5,6 +5,18 @@ import { action } from '@storybook/addon-actions';
 import * as O from 'fp-ts/Option';
 import AWSSetting from '../features/settings/AWSSetting';
 
-storiesOf('Setting', module).add('enabled', () => (
-  <AWSSetting config={O.none} onSubmit={action('onSubmit')} />
-));
+storiesOf('Setting', module)
+  .add('without existing config', () => (
+    <AWSSetting config={O.none} onSubmit={action('onSubmit')} />
+  ))
+  .add('with existing config', () => (
+    <AWSSetting
+      config={O.some({
+        accessId: '110111',
+        secretAccessKey: 'passwordd',
+        bucket: 'test-bucket',
+        region: 'ap-northeast-1',
+      })}
+      onSubmit={action('onSubmit')}
+    />
+  ));

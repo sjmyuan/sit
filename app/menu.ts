34d3efc5fde5a4aab42aea9d92ed1879eefcd5b7
Jiaming Shang <shangjiaming.yuan@gmail.com>
2021-01-03 20:39:19 +0800

    update readme and app information

--- app/menu.ts
+++ app/menu.ts
@@ -54,17 +54,17 @@ export default class MenuBuilder {
 
   buildDarwinTemplate(): MenuItemConstructorOptions[] {
     const subMenuAbout: DarwinMenuItemConstructorOptions = {
-      label: 'Electron',
+      label: 'Sit',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Sit',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Sit',
           accelerator: 'Command+H',
           selector: 'hide:',
         },

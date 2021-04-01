80ffa9ffe5bf4c46312336f63eb0d837de83fb94
Jiaming Shang <shangjiaming.yuan@gmail.com>
2021-04-01 13:26:59 +0800

    set canvas size according to image size

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as O from 'fp-ts/Option';
 import { Box, makeStyles } from '@material-ui/core';
-import { Stage, Layer, Rect } from 'react-konva';
+import { Stage, Layer, Rect, Image } from 'react-konva';
 import { Stage as KonvaStage } from 'konva/types/Stage';
 import { RectsContainer } from '../renderer/store-unstated';
 
@@ -28,6 +29,16 @@ const EditorPage = (): React.ReactElement => {
   const classes = useStyles();
   const rects = RectsContainer.useContainer();
   const [isDrawing, toggleDrawing] = useState<boolean>(false);
+  const [backgroundImg, setBackgroundImg] = useState<
+    O.Option<HTMLImageElement>
+  >(O.none);
+
+  useEffect(() => {
+    const image = new window.Image();
+    image.src =
+      'https://images.shangjiaming.com/16d67370-f68f-4413-85df-f2751286c53d.png';
+    image.addEventListener('load', () => setBackgroundImg(O.some(image)));
+  });
   return (
     <Box
       sx={{
@@ -35,44 +46,53 @@ const EditorPage = (): React.ReactElement => {
         border: '1px solid red',
       }}
     >
-      <Stage
-        className={classes.konva}
-        width={400}
-        height={400}
-        onMouseDown={(e) => {
-          const stage = e.target.getStage();
-          if (stage && !isDrawing) {
-            toggleDrawing(true);
-            const point = getRelativePointerPosition(stage);
-            const rect = {
-              id: rects.rects.length + 1,
-              origin: point,
-              width: 0,
-              height: 0,
-            };
-            rects.add(rect);
+      {O.isSome(backgroundImg) ? (
+        <Stage
+          className={classes.konva}
+          width={backgroundImg.value.width}
+          height={backgroundImg.value.height}
+          onMouseDown={(e) => {
+            const stage = e.target.getStage();
+            if (stage && !isDrawing) {
+              toggleDrawing(true);
+              const point = getRelativePointerPosition(stage);
+              const rect = {
+                id: rects.rects.length + 1,
+                origin: point,
+                width: 0,
+                height: 0,
+              };
+              rects.add(rect);
+              console.log(rects.rects);
+            }
+          }}
+          onMouseMove={(e) => {
+            const stage = e.target.getStage();
+            if (isDrawing && stage) {
+              rects.updateLast(getRelativePointerPosition(e.target.getStage()));
+            }
+          }}
+          onMouseUp={() => {
+            if (!isDrawing) {
+              return;
+            }
             console.log(rects.rects);
-          }
-        }}
-        onMouseMove={(e) => {
-          const stage = e.target.getStage();
-          if (isDrawing && stage) {
-            rects.updateLast(getRelativePointerPosition(e.target.getStage()));
-          }
-        }}
-        onMouseUp={() => {
-          if (!isDrawing) {
-            return;
-          }
-          console.log(rects.rects);
-          rects.clearEmpty();
-          toggleDrawing(false);
-        }}
-      >
-        <Layer>
-          {rects.rects.map((rect) => {
-            return (
-              <React.Fragment>
+            rects.clearEmpty();
+            toggleDrawing(false);
+          }}
+        >
+          <Layer>
+            <Image
+              x={0}
+              y={0}
+              width={backgroundImg.value.width}
+              height={backgroundImg.value.height}
+              image={O.toUndefined(backgroundImg)}
+            />
+          </Layer>
+          <Layer>
+            {rects.rects.map((rect) => {
+              return (
                 <Rect
                   key={`rect-${rect.id}`}
                   x={rect.origin.x}
@@ -83,11 +103,13 @@ const EditorPage = (): React.ReactElement => {
                   stroke="red"
                   fill="transparent"
                 />
-              </React.Fragment>
-            );
-          })}
-        </Layer>
-      </Stage>
+              );
+            })}
+          </Layer>
+        </Stage>
+      ) : (
+        <Stage className={classes.konva} width={400} height={400} />
+      )}
     </Box>
   );
 };

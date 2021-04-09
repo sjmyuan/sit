6e448147eae66be54af6ae30750614cae47bf814
Jiaming Shang <shangjiaming.yuan@gmail.com>
2021-04-09 21:56:31 +0800

    add worker

--- pages/worker.tsx
+++ pages/worker.tsx
@@ -2,28 +2,31 @@ import React from 'react';
 import * as A from 'fp-ts/Array';
 import * as TE from 'fp-ts/TaskEither';
 import { AppErrorOr } from '../renderer/types';
-import { loadImages } from '../renderer/utils/localImages';
+import { loadImages, getImageCache } from '../renderer/utils/localImages';
 import { useSelector } from 'react-redux';
 import { selectAWSConfig } from '../renderer/store';
 import { S3 } from 'aws-sdk';
-import { pipe } from 'fp-ts/lib/function';
+import { pipe, constVoid } from 'fp-ts/lib/function';
 import { uploadImage, deleteImage } from '../renderer/utils/remoteImages';
+import { ImageIndex } from '../renderer/utils/AppDB';
 
-const startWorker = (s3: S3, bucket: string): AppErrorOr<void> => {
-  const processingImages = loadImages(['ADDING', 'DELETING']);
+const startWorker = (s3: S3, bucket: string): AppErrorOr<void> =>
   pipe(
-    processingImages,
-    A.traverse(TE.taskEither)((image) => {
-      if (image.state === 'ADDING') {
-        const blob = getImageCache(image.key);
-        uploadImage(s3, bucket)(image.key, null);
-      } else {
-        deleteImage(image.key);
-      }
-    })
+    loadImages(['ADDING', 'DELETING']),
+    TE.chain((images) =>
+      A.traverse(TE.taskEither)((image: ImageIndex) => {
+        if (image.state === 'ADDING') {
+          return pipe(
+            getImageCache(image.key),
+            TE.chain((x) => uploadImage(s3, bucket)(image.key, x))
+          );
+        } else {
+          return deleteImage(s3, bucket)(image.key);
+        }
+      })(images)
+    ),
+    TE.map(constVoid)
   );
-};
-
 const Worker = (): React.ReactElement => {
   const awsConfig = useSelector(selectAWSConfig);
   return <div />;
--- renderer/utils/localImages.ts
+++ renderer/utils/localImages.ts
@@ -1,34 +1,46 @@
+import * as TE from 'fp-ts/TaskEither';
+import { constVoid, pipe } from 'fp-ts/lib/function';
 import { db, ImageIndex } from './AppDB';
-export const loadImages = (states: string[]): Promise<ImageIndex[]> =>
-  db.localIndex
-    .where('state')
-    .anyOf(...states)
-    .toArray();
+import { AppErrorOr } from '../types';
+export const loadImages = (states: string[]): AppErrorOr<ImageIndex[]> =>
+  TE.fromTask(() =>
+    db.localIndex
+      .where('state')
+      .anyOf(...states)
+      .toArray()
+  );
 
-export const uploadImage = async (key: string, image: Blob): Promise<void> => {
-  await db.localIndex.add({
-    key: key,
-    lastModified: Date.now(),
-    state: 'ADDING',
-  });
+export const uploadImage = (key: string, image: Blob): AppErrorOr<void> =>
+  pipe(
+    TE.fromTask<Error, unknown>(() =>
+      db.localIndex
+        .add({
+          key,
+          lastModified: Date.now(),
+          state: 'ADDING',
+        })
+        .then(() => db.cache.add({ key, image }))
+    ),
+    TE.map(constVoid)
+  );
+export const deleteImage = (key: string): AppErrorOr<void> =>
+  pipe(
+    TE.fromTask<Error, unknown>(() =>
+      db.localIndex
+        .update(key, {
+          state: 'DELETING',
+        })
+        .then(() => db.cache.delete(key))
+    ),
+    TE.map(constVoid)
+  );
 
-  await db.cache.add({ key, image });
-};
+export const getImageUrl = (key: string): AppErrorOr<string> =>
+  TE.fromTask(() =>
+    db.cache
+      .get(key)
+      .then((imageCache) => URL.createObjectURL(imageCache.image))
+  );
 
-export const deleteImage = async (key: string): Promise<void> => {
-  await db.localIndex.update(key, {
-    state: 'DELETING',
-  });
-
-  await db.cache.delete(key);
-};
-
-export const getImageUrl = async (key: string): Promise<string> => {
-  const imageCache = await db.cache.get(key);
-  return URL.createObjectURL(imageCache.image);
-};
-
-export const getImageCache = async (key: string): Promise<Blob> => {
-  const imageCache = await db.cache.get(key);
-  return imageCache.image;
-};
+export const getImageCache = (key: string): AppErrorOr<Blob> =>
+  TE.fromTask(() => db.cache.get(key).then((imageCache) => imageCache.image));

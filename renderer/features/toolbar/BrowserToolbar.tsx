53c19c52160c7531097e8940b8ebd946f0e3e681
sjmyuan <shang.jiaming@163.com>
2022-04-20 00:03:25 +0800

    only support back in history

--- renderer/features/toolbar/BrowserToolbar.tsx
+++ renderer/features/toolbar/BrowserToolbar.tsx
@@ -1,35 +1,8 @@
 import React from 'react';
 import { Box, IconButton } from '@mui/material';
-import { Crop, AddAPhoto } from '@mui/icons-material';
-import { ipcRenderer } from 'electron';
-import { ChangeEvent } from 'react';
-import { pipe } from 'fp-ts/lib/function';
-import * as A from 'fp-ts/Array';
-import * as TE from 'fp-ts/TaskEither';
-import { FileInfo } from '../../types';
-import { ImageContainer } from '../../store/ImageContainer';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 const BrowserToolbar = (): React.ReactElement => {
-  const imageContainer = ImageContainer.useContainer();
-  const handleUploadFileImage = (event: ChangeEvent<HTMLInputElement>) => {
-    const { files } = event.target;
-    if (files) {
-      const uploadingImages = pipe(
-        A.range(0, files.length - 1),
-        A.reduce<number, FileInfo[]>([], (acc, ele) => {
-          const file = files[ele];
-          if (file) {
-            return [...acc, { name: file.name, content: file }];
-          }
-          return acc;
-        })
-      );
-
-      A.array.traverse(TE.taskEither)(uploadingImages, (file) =>
-        imageContainer.addImage(file.name, file.content)
-      )();
-    }
-  };
   return (
     <Box
       sx={{
@@ -38,25 +11,12 @@ const BrowserToolbar = (): React.ReactElement => {
         alignItems: 'center',
       }}
     >
-      <IconButton color="inherit" aria-label="upload picture" component="label">
-        <AddAPhoto />
-        <input
-          accept="image/*"
-          hidden
-          id="icon-button-file"
-          type="file"
-          multiple
-          onChange={handleUploadFileImage}
-        />
-      </IconButton>
       <IconButton
         color="inherit"
-        aria-label="screen capture"
-        onClick={() => {
-          ipcRenderer.send('taking-screen-shot');
-        }}
+        aria-label="back to editor"
+        onClick={() => {}}
       >
-        <Crop />
+        <ArrowBackIosNewIcon />
       </IconButton>
     </Box>
   );

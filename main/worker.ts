0a78b840ac38f1f28405ca8c0ca10182fc50aa63
sjmyuan <shang.jiaming@163.com>
2022-05-16 22:30:53 +0800

    ask for permission once

--- main/worker.ts
+++ main/worker.ts
@@ -1,7 +1,25 @@
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, systemPreferences } from 'electron';
+import log from 'electron-log';
 import { loadRoute } from './util/routes';
 
 let workerWindow: BrowserWindow | null = null;
+
+const hasScreenAccess = (): boolean => {
+  try {
+    if (process.platform !== 'darwin') {
+      return true;
+    }
+
+    const status = systemPreferences.getMediaAccessStatus('screen');
+
+    log.info('Current screen access status:', status);
+
+    return status === 'granted';
+  } catch (error) {
+    log.error('Could not get screen permission:', error);
+  }
+  return false;
+};
 
 // eslint-disable-next-line import/prefer-default-export
 export const openWorkerWindow = async (): Promise<void> => {
@@ -26,6 +44,7 @@ export const prepareForCropperWindow = async (
   if (workerWindow) {
     workerWindow.webContents.send('worker_prepare-for-cropper-window', {
       takeFullScreenShot: takeFullScreenShot,
+      hasPermission: hasScreenAccess(),
     });
   }
 };
--- pages/worker.tsx
+++ pages/worker.tsx
@@ -1,22 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as O from 'fp-ts/Option';
 import { ipcRenderer } from 'electron';
 import { getVideo, takeShot } from '../renderer/utils/screen';
 
 const Worker = () => {
+  const [checkedPermission, setCheckedPermission] = useState<boolean>(false);
+
   useEffect(() => {
     ipcRenderer.on(
       'worker_prepare-for-cropper-window',
-      async (_, { takeFullScreenShot }: { takeFullScreenShot: boolean }) => {
+      async (
+        _,
+        {
+          takeFullScreenShot,
+          hasPermission,
+        }: { takeFullScreenShot: boolean; hasPermission: boolean }
+      ) => {
         const video = await getVideo();
         const buffer = await takeShot(O.none, video);
-        ipcRenderer.send('main_open-cropper-window', {
-          fullScreen: buffer,
-          takeFullScreenShot,
-        });
+
+        // only ask for permission once
+        if (hasPermission || checkedPermission) {
+          ipcRenderer.send('main_open-cropper-window', {
+            fullScreen: buffer,
+            takeFullScreenShot,
+          });
+        }
+        setCheckedPermission(true);
       }
     );
-  }, []);
+  }, [checkedPermission]);
   return <div>Worker</div>;
 };
 

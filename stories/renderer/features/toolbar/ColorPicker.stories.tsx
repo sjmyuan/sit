d9b55d0eff17db32a52818e972d693538481a21c
sjmyuan <shang.jiaming@163.com>
2022-05-31 22:29:17 +0800

    add ColorPicker story

---
+++ stories/renderer/features/toolbar/ColorPicker.stories.tsx
@@ -0,0 +1,8 @@
+import { ComponentMeta } from '@storybook/react';
+
+import ColorPicker from '../../../../renderer/features/toolbar/ColorPicker';
+
+export default {
+  title: 'ColorPicker',
+  component: ColorPicker,
+} as ComponentMeta<typeof ColorPicker>;

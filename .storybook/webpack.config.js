1e825cfb1200d4c770dc189e10bad79bb8936fbe
sjmyuan <shang.jiaming@163.com>
2022-05-31 21:22:49 +0800

    enable storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,14 @@
-module.exports = ({config}) => {
+module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
       {
-        loader: require.resolve('awesome-typescript-loader'),
+        loader: require.resolve('ts-loader'),
       },
-      // Optional
-      {
-        loader: require.resolve('react-docgen-typescript-loader'),
-      },
+      // // Optional
+      // {
+      //   loader: require.resolve('react-docgen-typescript-loader'),
+      // },
     ],
   });
   config.resolve.extensions.push('.ts', '.tsx');

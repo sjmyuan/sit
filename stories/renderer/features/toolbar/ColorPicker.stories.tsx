2b0a96f25e68589f02048f2c7e75d42aadcae014
sjmyuan <shang.jiaming@163.com>
2022-05-31 22:39:50 +0800

    fix colorpircker story

--- stories/renderer/features/toolbar/ColorPicker.stories.tsx
+++ stories/renderer/features/toolbar/ColorPicker.stories.tsx
@@ -1,4 +1,6 @@
-import { ComponentMeta } from '@storybook/react';
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import ColorPicker from '../../../../renderer/features/toolbar/ColorPicker';
 
@@ -6,3 +8,7 @@ export default {
   title: 'ColorPicker',
   component: ColorPicker,
 } as ComponentMeta<typeof ColorPicker>;
+
+export const Primary: ComponentStory<typeof ColorPicker> = () => (
+  <ColorPicker color={'red'} onChange={(color) => {}}></ColorPicker>
+);
